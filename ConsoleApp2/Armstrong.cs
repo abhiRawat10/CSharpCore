922785d95980a17289f42433dccd6ce79407be52
abhirawat10 <abhi10rawat2002@gmail.com>
2025-02-15 12:02:39 +0530

    final

--- ConsoleApp2/Armstrong.cs
+++ ConsoleApp2/Armstrong.cs
@@ -1,8 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-public class Class1
+namespace cSharpCore
 {
-	public Class1()
-	{
-	}
+    internal class Prime
+    {
+        public static void Entry()
+        {
+            Console.WriteLine("Enter a number to check if it is Armstrong or not");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(isArmstrong(n));
+        }
+
+        public static bool isArmstrong(int n)
+        {
+            int originalNumber = n, sum = 0, digits = n.ToString().Length;
+
+            while (n > 0)
+            {
+                int digit = number % 10;
+                sum += (int)Math.Pow(digit, digits);
+                n /= 10;
+            }
+
+            return sum == originalNumber;
+        }
+    }
 }
--- ConsoleApp2/IfElse.cs
+++ ConsoleApp2/IfElse.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-public class Class1
+namespace cSharpCore
 {
-	public Class1()
-	{
-	}
+    internal class Prime
+    {
+        public static void Entry()
+        {
+            Console.WriteLine("Enter a number to check if it is Positive or not");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0) Console.WriteLine("positive");
+            else if (n < 0) Console.WriteLine("negative");
+            else Console.WriteLine("Zero"); 
+        }
+    }
 }
