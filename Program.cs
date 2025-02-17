1cbc304235540f2d4f30ac4c1ceaf9b608c131d8
abhirawat10 <abhi10rawat2002@gmail.com>
2025-02-17 22:35:22 +0530

    inital commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Operation");
+            string operation = Console.ReadLine();
+
+            switch(operation)
+            {
+                case "+":
+                    Console.WriteLine("The sum is: " + (a + b));
+                    break;
+                case "-":
+                    Console.WriteLine("The difference is: " + (a - b));
+                    break;
+                case "*":
+                    Console.WriteLine("The product is: " + (a * b));
+                    break;
+                case "/":
+                    Console.WriteLine("The quotient is: " + (a / b));
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operation");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
